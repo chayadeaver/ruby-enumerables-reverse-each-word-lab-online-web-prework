1d763a24dda0facc931b6080fec5704364249467
Chaya Deaver <chayadeaver@gmail.com>
2019-08-07 01:12:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
-def reverse_each_word()
-reversed_string = ""
-i = 0 
-  while i < string.length do
-    reversed_string = string[i] + reversed_string
-    i+=1
- end
+def reverse_each_word(string)
+
 end
