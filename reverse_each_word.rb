1411b81e1fb347a1f2570b9edd90c239549e771b
Chaya Deaver <chayadeaver@gmail.com>
2019-08-07 02:06:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 require "pry"
 def reverse_each_word(string)
 array = string.split(" ")
- reversed_string = []
  
- array.each do |string|
-   reversed_string << string.reverse
+ reversed_string = array.collect do |string|
+   string.reverse
  end
  
  return reversed_string.join(" ")
