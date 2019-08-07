c4bc298a3afd5c5fea56aa5b9096e91caa8a9e27
Chaya Deaver <chayadeaver@gmail.com>
2019-08-07 02:03:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
+require "pry"
 def reverse_each_word(string)
 array = string.split("  ")
  reversed_string = []
  
  array.each do |string|
+   binding.pry
    reversed_string << string.reverse
  end
  
